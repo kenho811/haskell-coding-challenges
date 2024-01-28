dcec0e974489363b217698405b97826ef0f3c241
kenho <kenho811@gmail.com>
2024-01-28 14:23:12 +0800

    finish day4 part2

--- src/AdventOfCode/2023/Day4/Part2/Solution.hs
+++ src/AdventOfCode/2023/Day4/Part2/Solution.hs
@@ -1,75 +1,90 @@
-module AdventOfCode.Day3.Part1.Solution where
-import Data.Char (isDigit)
-import Data.List (nub)
+module AdventOfCode.Day4.Part1.Solution where
+import Text.ParserCombinators.Parsec
+import Control.Monad (forM_)
+import qualified Data.IntMap as M
+import Data.Either (fromRight)
+import Data.Functor.Classes (Show1(liftShowsPrec))
 
 
-type Position = (Row, Column)
-type Pair = (Integer, Position)
-type Column = Integer
-type Row = Integer
-
-
-solution :: IO ()
+solution:: IO ()
 solution = do
-    content <- readFile "src/AdventOfCode/2023/Day3/Part2/input.txt"
-    let ls = lines content
-    let digitPos = digitPositions ls
-    let symbolPos = symbolPositions ls
-    let list = filter (\l -> length l ==2) $ map (isAround digitPos) symbolPos
-    print $ sum $ map product list
+    content <- readFile "src/AdventOfCode/2023/Day4/Part1/input.txt"
+    let eithers = map parseLine $ lines content
+    forM_ eithers printCard
+    let cards = getCards eithers
+    let initialMap = initMap cards
+    let finalMap = totalNumScratchCards cards initialMap
+    print finalMap
+    print $ M.foldl' (+) 0 finalMap
 
-isAround:: [(Integer, [Position])] -> Position -> [Integer]
-isAround [] _ = []
-isAround ((i, xs):ys) p = let corners = getSingleCorners p in
-    if any (`elem` corners) xs then i: isAround ys p else isAround ys p
-                    
+defaultCard :: Card
+defaultCard = Card {
+    cardId= -99999
+    , winningNumbers=[-1]
+    , existingNumbers=[-1]
+    , numMatches=0
+}
 
 
+printCard :: Either ParseError Card -> IO()
+printCard (Left err) = putStrLn $ "Parsing Failed: " ++ show err
+printCard (Right card) = print card
 
--- get symbols
-symbolPositions :: [String] -> [Position]
-symbolPositions = symbolPositionsFromStrings 0
-
-symbolPositionsFromStrings :: Row -> [String] -> [Position]
-symbolPositionsFromStrings _ [] = []
-symbolPositionsFromStrings n (x:xs)= symbolPositionsFromString n x ++ symbolPositionsFromStrings (n+1) xs
-
-symbolPositionsFromString :: Row -> String -> [Position]
-symbolPositionsFromString row line = [(row,col)| (char,col) <- zip line [0..], not $ isDigit char, char == '*']
-
+getCards:: [Either ParseError Card] -> [Card]
+getCards = map (fromRight defaultCard)
 
 
 
--- get digits
-digitPositions :: [String] -> [(Integer, [Position])]
-digitPositions =  digitPositionsFromStrings 0 
-
-digitPositionsFromStrings :: Row -> [String] -> [(Integer, [Position])]
-digitPositionsFromStrings _ [] = []
-digitPositionsFromStrings n (x:xs)= digitsToNumbers (mergeDigits $ digitWithPosition n x) ++ digitPositionsFromStrings (n+1) xs
+initMap :: [Card] -> M.IntMap Int
+initMap cs = M.fromList [let cId = cardId c in (cId , 1)| c <- cs]
 
 
-digitWithPosition:: Row -> String -> [(String, [Position])]
-digitWithPosition row line = [([char],[(row, col)])| (char,col) <- zip line [0..], isDigit char]
+totalNumScratchCards :: [Card] -> M.IntMap Int -> M.IntMap Int
+totalNumScratchCards [] m = m
+totalNumScratchCards (x:xs) m =
+     case M.lookup (cardId x) m of
+        Nothing -> m
+        Just num -> let newMap = addToMap m (cardId x + 1) (numMatches x) num in totalNumScratchCards xs newMap
 
-mergeDigits :: [(String, [Position])] -> [(String, [Position])]
-mergeDigits [] = []
-mergeDigits [(x, y)] = [(x,y)]
-mergeDigits ((a, xs): (c,ys): ss)
-    | rightColumn == leftColumn +1 = mergeDigits ((a ++ c,xs ++ ys) : ss)
-    | otherwise = (a,xs): mergeDigits ((c,ys) : ss)
-    where leftColumn = snd $ last xs
-          rightColumn = snd $ head ys
-
-digitsToNumbers :: [(String, [Position])] -> [(Integer, [Position])]
-digitsToNumbers combos = [(read x,y) | (x, y) <- combos ]
+addToMap :: M.IntMap Int -> Int -> Int -> Int -> M.IntMap Int
+addToMap m curr cnt mul
+    | cnt <= 0 = m
+    | otherwise = let newMap = M.insertWith (+) curr mul m in addToMap newMap (curr + 1) (cnt -1) mul
 
 
+data Card = Card {
+    cardId:: Int
+    ,winningNumbers:: [Int]
+    ,existingNumbers:: [Int]
+    ,numMatches:: Int
+} deriving Show
 
--- get position corner
-getSingleCorners:: Position -> [Position]
-getSingleCorners (x, y) = [(x', y') |
-                       x' <- [x - 1, x, x + 1], x' >= 0, y' <- [y - 1, y, y + 1], y' >= 0, (x',y') /= (x,y)]
 
-getMultiCorners:: [Position] -> [Position]
-getMultiCorners = nub . concatMap getSingleCorners
+numberParser:: Parser Int
+numberParser = read <$> many1 digit
+
+numberListParser :: Parser [Int]
+numberListParser = sepBy1 numberParser spaces
+
+
+
+cardParser :: Parser Card
+cardParser = do
+    _ <- string "Card" >> spaces
+    cardId <- numberParser
+    _ <- string ":" >> spaces
+    winningNumbers <- numberListParser
+    _ <- string "|" >> spaces
+    existingNumbers <- numberListParser
+    let numMatches = length [x | x<- existingNumbers, y<-winningNumbers, x == y]
+    return $ Card {
+        cardId=cardId
+        , winningNumbers=winningNumbers
+        , existingNumbers=existingNumbers
+        , numMatches=numMatches
+    }
+
+parseLine :: String -> Either ParseError Card
+parseLine = parse cardParser "WRONG"
+
+
