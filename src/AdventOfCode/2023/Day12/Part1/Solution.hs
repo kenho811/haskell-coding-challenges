210cd18ebeeafcc44b709a13934dbc94287c57d6
kenho <kenho811@gmail.com>
2024-02-16 11:37:53 +0800

    complete Day12 Part1

--- src/AdventOfCode/2023/Day12/Part1/Solution.hs
+++ src/AdventOfCode/2023/Day12/Part1/Solution.hs
@@ -1,30 +1,52 @@
 module AdventOfCode.Day12.Part1.Solution where
 
 import qualified Data.Text as T
+import Data.List.GroupBy ( groupBy ) 
 
 
 solution :: IO ()
 solution = do
     content <- readFile "src/AdventOfCode/2023/Day12/Part1/input.txt"
+    -- content <- readFile "src/AdventOfCode/2023/Day12/Part1/sample.txt"
     let 
-        conditionsAndBrokenNums :: [([Condition], BrokenNum)]
-        conditionsAndBrokenNums = zip conditions brokenNums
+        conditionsAndBrokenNumsList :: [(Conditions, BrokenNums)]
+        conditionsAndBrokenNumsList = zip conditionsList brokenNumsList
 
-        conditions :: [[Condition]]
-        conditions = map (map parseCondition . head . words) . lines $ content
+        conditionsList :: [Conditions]
+        conditionsList = map (map parseCondition . head . words) . lines $ content
 
-        brokenNums :: [BrokenNum]
-        brokenNums = map (map (read . T.unpack) . T.splitOn (T.pack ",") . last . T.words) . T.lines . T.pack $ content
+        brokenNumsList :: [BrokenNums]
+        brokenNumsList = map (map (read . T.unpack) . T.splitOn (T.pack ",") . last . T.words) . T.lines . T.pack $ content
+
+        numMatchingPossibilities :: [Int]
+        numMatchingPossibilities =  map (uncurry getNumMatches) conditionsAndBrokenNumsList
+
 
     -- print conditions
     -- print brokenNums
-    print conditionsAndBrokenNums
+    -- print .  map (generatePossibleConditions . fst) $ conditionsAndBrokenNumsList
+    print  numMatchingPossibilities
+    print . sum $ numMatchingPossibilities
 
 
-type BrokenNum = [Int]
+type Conditions = [Condition]
+type BrokenNums = [Int]
 data Condition = Operational
                  | Broken
-                 | Unknown deriving Show
+                 | Unknown deriving (Show, Eq)
+
+getNumMatches :: Conditions -> BrokenNums -> Int
+getNumMatches cs targetBrokenNums = length . filter (==targetBrokenNums) $ possibleBrokenNums
+    where possibleConditions = generatePossibleConditions cs
+          possibleBrokenNums = map getBrokenNum possibleConditions  
+
+
+getBrokenNum :: Conditions -> BrokenNums
+getBrokenNum = map length 
+              . groupBy (\a b -> b == a+1 ) 
+              . map fst 
+              . filter (\(_, c) -> c == Broken ) 
+              .  zip [0..]
 
 parseCondition :: Char -> Condition
 parseCondition '.' = Operational
@@ -32,10 +54,19 @@ parseCondition '#' = Broken
 parseCondition '?' = Unknown
 parseCondition x = error $ "Unknown Condition " ++ show x
 
-generateRecords :: [Condition] -> [[Condition]]
-generateRecords  = map reverse . foldr (
+generatePossibleConditions :: [Condition] -> [[Condition]]
+generatePossibleConditions  = foldr (
     \c acc -> case c of
         Unknown -> [ c': xs | xs <- acc, c' <- [Operational, Broken]]
         _ -> [c: xs | xs <- acc]
     ) [[]]
 
+
+simpleTest :: IO ()
+simpleTest = do
+    let conditions = map parseCondition "???.###"
+        possibleConditions = generatePossibleConditions conditions
+        possibleBrokenNums = map getBrokenNum possibleConditions
+    print conditions
+    print possibleConditions
+    print possibleBrokenNums
