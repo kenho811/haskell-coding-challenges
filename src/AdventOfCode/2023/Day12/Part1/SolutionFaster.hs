b63d0e7707a345adce635bccd816414b1ec69772
kenho <kenho811@gmail.com>
2024-02-16 22:54:06 +0800

    still faster

--- src/AdventOfCode/2023/Day12/Part1/SolutionFaster.hs
+++ src/AdventOfCode/2023/Day12/Part1/SolutionFaster.hs
@@ -21,7 +21,7 @@ solution = do
         brokenNumsList = map (map (read . T.unpack) . T.splitOn (T.pack ",") . last . T.words) . T.lines . T.pack $ content
 
         numMatchingPossibilities :: [Int]
-        numMatchingPossibilities =  map (\(cs, ns) -> length $ getMatches cs ns 0 [] ) conditionsAndBrokenNumsList
+        numMatchingPossibilities =  map (\(cs, ns) -> getNumMatches cs ns 0 ) conditionsAndBrokenNumsList
 
     -- print  conditionsList
     -- print  conditionsList'
@@ -46,17 +46,17 @@ parseCondition '?' = Unknown
 parseCondition x = error $ "Unknown Condition " ++ show x
 
 
-getMatches :: Conditions -> BrokenNums -> Int -> Conditions -> [Conditions]
-getMatches [] ns _ acc  = [reverse acc | null ns]
-getMatches cs [] _  acc = [reverse (cs ++ acc) | Broken `notElem` cs]
-getMatches (c:cs) (n:ns) cnt acc = case c of
+getNumMatches :: Conditions -> BrokenNums -> Int -> Int
+getNumMatches [] ns _   = if null ns then 1 else 0
+getNumMatches cs [] _   = if Broken `notElem` cs then 1 else 0
+getNumMatches (c:cs) (n:ns) cnt  = case c of
     Operational ->  if cnt == 0 -- no previous broken states. Continue
-                    then  getMatches cs (n:ns) 0 (c:acc)
+                    then  getNumMatches cs (n:ns) 0
                     else
                         if cnt == n  -- check if previous non-zero continguous BrokenNum matches expected one
-                        then getMatches cs ns 0 (c:acc) -- continue
-                        else [] -- Not Match. Therefore abort.
-    Broken -> getMatches cs (n:ns) (cnt + 1) (c:acc)
-    Unknown -> getMatches (Operational:cs) (n:ns) cnt acc ++ getMatches (Broken:cs) (n:ns) cnt acc
+                        then getNumMatches cs ns 0  -- continue
+                        else 0 -- Not Match. Therefore abort.
+    Broken -> getNumMatches cs (n:ns) (cnt + 1)
+    Unknown -> getNumMatches (Operational:cs) (n:ns) cnt  + getNumMatches (Broken:cs) (n:ns) cnt
 
 
